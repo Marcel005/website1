523d401808f7a6a0ebdaf9ea3caac519c1dce447
Marcel Leal <142708094+Marcel005@users.noreply.github.com>
2024-02-27 10:10:22 -0800

    Loading

--- js/loading.js
+++ js/loading.js
@@ -1,5 +1,18 @@
 loadText = document.querySelector('.loading-text')
+bg = document.querySelector('.bg')
 
 load = 0
 
-loadText.innerText = load
+int = setInterval(blurring, 30)
+
+function blurring() {
+    load++
+    if (load > 99) {
+        clearInterval(int)
+    }
+    loadText.innerText = `${load}%`
+
+
+    loadText.style.opacity = 1 - load/100
+    bg.style.filter = `blur(${30 - (load/100)*30}px)`
+}
