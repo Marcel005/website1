8ea5608a97ef8504385e1b8581f99c8f8ddf0e4c
Marcel Leal <142708094+Marcel005@users.noreply.github.com>
2024-03-13 16:22:35 +0000

    Wed, Mar 13, 2024, 9:22 AM -07:00

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ const FACING_UP = 1;
 const FACING_LEFT = 2;
 const FACING_RIGHT = 3;
 const FRAME_LIMIT = 12;
-const MOVEMENT_SPEED = 3;
+const MOVEMENT_SPEED = 4;
 
 let canvas = document.querySelector('canvas');
 let ctx = canvas.getContext('2d');
